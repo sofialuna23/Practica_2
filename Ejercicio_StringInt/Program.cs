1341cf99039f75ac1ae5794609f384b6c3007e08
Sofia Luna <Sofia Luna@LAPTOP-KDK0IDS2>
2021-08-31 22:28:42 -0700

    Se agrego ejercicio String-Int

---
+++ Ejercicio_StringInt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            String dato = "";
+            int edad = 0 ; 
+
+            Console.WriteLine("Ingrese su nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad:");
+            dato = Console.ReadLine();
+            edad = Convert.ToInt32(dato);
+            Console.WriteLine("\n Te llamas " + nombre + " y tienes {0}", edad + " años ");
+            Console.WriteLine("\n \n -- Presiona cualquier tecla para continuar --");
+            Console.ReadKey();
+        }
+    }
+}
